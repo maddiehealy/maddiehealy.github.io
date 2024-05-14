c25760300f94729ba1f7778863c919b22762164a
Maddie Healy <maddie3470@gmail.com>
2024-05-14 16:56:40 -0500

    more debugging

--- technical_07.js
+++ technical_07.js
@@ -1,66 +1,52 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const items = [
-        { uuid: '6f18e3a7-a396-46d9-85cb-92674c24cfc0', containerSelector: '#item-details' },
-        { uuid: '50f7b9a5-329a-49ab-85e2-f8fb4ee6e867', containerSelector: '#item-details' }
-    ];
+document.addEventListener('DOMContentLoaded', () => {
+    window.addEventListener('load', loadXML);
+  })
+  
+  var parentElement = document.getElementById('ochreTableBody');
+  var url = "https://ochre.lib.uchicago.edu/ochre?uuid=6f18e3a7-a396-46d9-85cb-92674c24cfc0";
+  
+  function loadXML() {
+    fetch(url)
+    .then(response => response.text())
+    .then(data =>
+      parseData(data)
+    )
+    .catch(error => {
+      console.error('Error loading XML file:', error);
+    })
+  };
 
-    items.forEach(({ uuid, containerSelector }) => {
-        const url = `https://ochre.lib.uchicago.edu/ochre?uuid=${uuid}`;
-        fetch(url)
-            .then(response => {
-                if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
-                return response.text();
-            })
-            .then(xmlData => {
-                const xmlDoc = parseData(xmlData);
-                updateDocumentContent(xmlDoc, containerSelector);
-                updateImageContent(xmlDoc);  // Handles displaying of images if found in the XML
-                loadImage(url);  // Function call to load image added
-            })
-            .catch(error => {
-                console.error('Error loading XML data:', error);
-                document.querySelector(containerSelector).innerHTML = `<p>Error loading item details: ${error.message}</p>`;
-            });
-    });
-});
-
-function parseData(xmlData) {
+  function parseData(xmlData){
     const parser = new DOMParser();
     const xmlDoc = parser.parseFromString(xmlData, 'application/xml');
-    if (xmlDoc.documentElement.nodeName === "parsererror") {
-        throw new Error("Error parsing XML data.");
-    }
-    return xmlDoc;
-}
-
-function updateDocumentContent(xmlDoc, containerSelector) {
-    var itemTitle = xmlDoc.querySelector("title") ? xmlDoc.querySelector("title").textContent : "Title Not Found";
-    var itemDescription = xmlDoc.querySelector("description") ? xmlDoc.querySelector("description").textContent : "Description Not Available";
-    var license = xmlDoc.querySelector("license") ? xmlDoc.querySelector("license").textContent : "License Info Not  Not Available";
-    
-    let html = `<h2>${itemTitle}</h2>
-        <p>Description: ${itemDescription}</p>
-        <p>License: ${license}</p>`;
-
-    var container = document.querySelector(containerSelector);
-    if (container) {
-        container.innerHTML = html;
-    } else {
-        console.error("Container not found for selector: " + containerSelector);
-    }
-}
-
-function updateImageContent(xmlDoc) {
-    const previewImage = xmlDoc.querySelector("previewImage") ? xmlDoc.querySelector("previewImage").getAttribute("src") : null;
-    if (previewImage) {
-        const imageContainer = document.getElementById('imageOutput');
-        imageContainer.innerHTML = `<img src="${previewImage}" alt="Preview Image" class="img-fluid">`;
-    }
-}
-
-function loadImage(url){
-    let newIMG = document.createElement("img");
-    newIMG.src = url + "&preview";
-    newIMG.className = "img-fluid";
-    document.getElementById('imageOutput').appendChild(newIMG);
-}
+    createHeaders(xmlDoc),
+    listProps(xmlDoc);
+  };
+  
+  function createHeaders(sourceXML){
+    document.getElementById('projectTitle').innerText = sourceXML.getElementsByTagName('metadata')[0].children[1].innerHTML;
+    document.getElementById('itemTitle').innerText = sourceXML.getElementsByTagName('spatialUnit')[0].children[3].children[0].children[0].children[0].innerHTML;
+    document.getElementById('itemDescription').innerText = sourceXML.getElementsByTagName('spatialUnit')[0].children[4].children[0].children[0].innerHTML
+    var licenseText = document.getElementById('license');
+    licenseText.innerText = sourceXML.getElementsByTagName('availability')[0].children[0].innerHTML;
+  }
+  
+  function listProps(sourceXML){
+    var baseXPath = '/result/ochre/spatialUnit/observations/observation/properties/'
+    console.log(sourceXML);
+    var allProps = sourceXML.getElementsByTagName('properties');
+    showProp()
+    for (i=0; i < propList.length; i++) {
+      let newUL = document.createElement("ul");
+  
+      document.getElementById('itemLists').appendChild(newUL);
+    };
+  }
+  
+  function showProp(){
+  
+    let newLI = document.createElement("li");
+    newLI.innerHTML = propList[i].children[0].children[0].children[0].innerHTML + ": " + propList[i].children[1].innerHTML;
+    //populate the cells in the row
+    document.getElementById('itemDetails').appendChild(newLI);
+  }
